bfb1be5543c94989de3a62ee14057b7893301bef
Falk Hoppe <falkhoppe81@googlemail.com>
2012-12-07 18:10:32 +0100

    initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RocaTodo::Application.config.secret_token = 'd3b8a525294134e14829e11b434a32085ccf6735ba1800ec6a249a4caceffdf30b126f1314bad2773653f5e49ff4cc79813ba3e94d2a862acd6ae0cc2036a473'
